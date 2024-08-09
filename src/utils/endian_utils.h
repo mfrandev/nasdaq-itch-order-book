56750cf473717667b715063f8bc04f9ca2f275c9
Michael <mfdev99@gmail.com>
2024-08-09 03:18:48 -0400

    Fix compile on MacBook

--- src/utils/endian_utils.h
+++ src/utils/endian_utils.h
@@ -2,11 +2,12 @@
 #define TREXQUANTTAKEHOME_UTILS_ENDIAN_UTILS_H_
 
 #include <cstdint>
+#include <stdlib.h>
 
 /**
  * The binary file stores integers big-endian, and x86 is little endian.
  * Here is a utility function to convery big-endian to little-endian
  */
-uint64_t toHostEndianUpTo64(const char* data, std::size_t size);
+uint64_t toHostEndianUpTo64(const char* data, size_t size);
 
 #endif // TREXQUANTTAKEHOME_UTILS_ENDIAN_UTILS_H_
